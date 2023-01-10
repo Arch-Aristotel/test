068490ed4a5880c65d3f07830ab27e40b3e3367b
Gremlen <9150939900@mail.ru>
2023-01-10 15:26:52 +0300

    test v01

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1234");
+
+        dell_simbyl e = (x) ->{
+            String tmp[] = x.split("a");
+            x=null;
+            for (String a : tmp){
+                if(x!=null) x=x+a;
+                    else x=a;
+            }
+            return x;
+        };
+
+        System.out.println(e.cleare_line("lonag liave ias goaod"));
     }
+}
+interface dell_simbyl{
+    String cleare_line(String clear);
 }
