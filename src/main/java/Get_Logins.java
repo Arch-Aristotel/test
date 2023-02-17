253131061e728a09421aa3f51604c4d8f5b622c5
Gremlen <9150939900@mail.ru>
2023-02-17 19:03:02 +0300

    test v05

---
+++ src/main/java/Get_Logins.java
@@ -0,0 +1,37 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+
+public class Get_Logins {
+    public List<String> list;
+
+    public void get_name(String pathX){
+        try {
+            Path pt = Paths.get(pathX);
+            list = Files.readAllLines(pt);
+        }
+        catch (Exception e){
+
+        }
+
+        }
+
+        public void rec_file(String path_input, String name){
+        name = name+"\n";
+        Path pt = Paths.get(path_input);
+            try {
+                byte[] strToBytes = name.getBytes();
+                Files.write(pt, strToBytes, StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,73 +1,47 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) throws Exception{
 
-        String a="qwejksdfdsfv 1234qwels very meny words6543qwels";
-        String b = a.substring(a.indexOf("1234qwels")+9, a.indexOf("6543qwels"));
-
-
         String name = null;
-        String nameParser_start = "Полное имя</span><div class=\"userValue\" data-attr=\"\"><span>";
-        String nameParser_end = "</span></div></div><div class=\"userField\">";
+        String Start = "<script id=\"dynamicJSONLD\" type=\"application/ld+json\">{\"@context\":\"https://schema.org\",\"@type\":\"Person\",\"alternateName\":";
+        String Stop = "\"}}</script>";
 
-        String Department = null;
-        String DepartmentParse_start = "userDepartmentContainer\" title=\"";
-        String DepartmentParse_end = "\"><a href=\"https://newportal.mts.ru/Pages/Org.aspx?openID";
 
-        String Mobile_number = null;
-        String Mobile_numberParse_start = "<div class=\"userMobileNumberContainer\"><span>";
-        String Mobile_numberParse_end = "</span><div class=\"userSmsButton\" data-phone=";
 
-        String Data_number = null;
-        String Data_numberParsr_start = "Рабочий телефон</span><div class=\"userValue usernumberFont\" data-attr=\"\">";
-        String Data_numberParsr_end ="</div></div><div class=\"userField userManagerField\"><span class=\"userLabel\">";
+        Scanner cin_file_logs = new Scanner(System.in);
 
-        String Login = null;
-        String Login_parser_start ="Логин</span><div class=\"userValue\" data-attr=\"\"><span>";
-        String Login_parser_end ="</span></div></div><div class=\"userField userAddressField\">";
-
-        String addres = null;
-        String addres_Parsrs_tart ="Адрес</span><div class=\"userValue\" data-attr=\"\"><span>";
-        String addres_Parsrs_end = "</span></div></div><div class=\"userField userRegionTimeZoneField\">";
-
-        String mail=null;
-        String mail_parser_start = "class=\"userMailContainer\"><a href=\"mailto:";
-        String mail_parser_end = "<div class=\"userEmailButton\"></div></a><a class=\"chatWithSkype-piwik";
+        Get_Logins st1 = new Get_Logins();
+        System.out.println("Input path to file whith UserName");
+        st1.get_name("D:\\Справочник МГТС\\List-OTUsever.txt");
+        System.out.println("Input path to file to safe");
+        Scanner cin_file_safe = new Scanner(System.in);
+        String PathX = cin_file_safe.nextLine();
 
 
 
 
-        URL url = new URL("file:///D:/java/terex2.html");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(), "UTF-8"));
-        while (true) {
-            String line = reader.readLine();
-            if (line == null) break;
+        for(String url_name : st1.list) {
 
-        try {
-
-            Login=line.substring(line.indexOf(Login_parser_start)+Login_parser_start.length(), line.indexOf(Login_parser_end));
-            name=line.substring(line.indexOf(nameParser_start)+nameParser_start.length(), line.indexOf(nameParser_end));
-            Data_number=line.substring(line.indexOf(Data_numberParsr_start)+Data_numberParsr_start.length(), line.indexOf(Data_numberParsr_end));
-            Mobile_number=line.substring(line.indexOf(Mobile_numberParse_start)+Mobile_numberParse_start.length(),line.indexOf(Mobile_numberParse_end));
-            Department=line.substring(line.indexOf(DepartmentParse_start)+DepartmentParse_start.length(), line.indexOf(DepartmentParse_end));
-            addres=line.substring(line.indexOf(addres_Parsrs_tart)+addres_Parsrs_tart.length(), line.indexOf(addres_Parsrs_end));
-            mail=line.substring(line.indexOf(mail_parser_start)+mail_parser_start.length(), line.indexOf(mail_parser_end));
-
+            System.out.println("https://newportal.mts.ru/my/Person.aspx?accountname=MGTS\\"+url_name);
+            URL url = new URL("https://newportal.mts.ru/my/Person.aspx?accountname=MGTS\\"+ url_name);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(), "UTF-8"));
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) break;
+                try {
+                    name = line.substring(line.indexOf(Start) + Start.length(), line.indexOf(Stop));
+                } catch (Exception e) {
+                }
+            }
+            st1.rec_file("D:\\Справочник МГТС\\test.txt", name);
         }
-        catch (Exception e) {
-        }
-
-
-
-        }
-        System.out.println(Login+" "+name+" "+Data_number+" "+Mobile_number+" "+Department+" "+addres+" "+mail);
-
-
-
-
-
     }
 }
